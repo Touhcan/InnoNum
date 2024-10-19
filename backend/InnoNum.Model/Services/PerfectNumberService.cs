a94ff22148f63ad7aadfd4015207af61738cbcab
Touhcan <brunner.sim@gmail.com>
2024-10-19 12:36:13 +0200

    Add some argument validation

--- backend/InnoNum.Model/Services/PerfectNumberService.cs
+++ backend/InnoNum.Model/Services/PerfectNumberService.cs
@@ -13,6 +13,24 @@ public class PerfectNumberService : IPerfectNumberService
 
     public PerfectNumberCount CountPerfectNumbersBetween(int minimum, int maximum)
     {
+        if (minimum >= maximum)
+        {
+            _logger.LogError(
+                "Invalid argument: {minimum} >= {maximum}",
+                minimum, maximum);
+            throw new ArgumentOutOfRangeException(
+                $"{nameof(minimum)} needs to be less than {nameof(maximum)}");
+        }
+
+        if (minimum <= 0 || maximum <= 0)
+        {
+            _logger.LogError(
+                "One argument is <= 0: minimum: {minimum}, maximum: {maximum}",
+                minimum, maximum);
+            throw new ArgumentOutOfRangeException(
+                "All arguments need to be greater than 0");
+        }
+
         var count = 0;
         for (var number = minimum; number < maximum; number++)
         {
@@ -34,6 +52,7 @@ public class PerfectNumberService : IPerfectNumberService
                 }
             }
         }
+
         return new(count);
     }
 }
--- backend/InnoNum.UnitTests/TestPerfectNumberService.cs
+++ backend/InnoNum.UnitTests/TestPerfectNumberService.cs
@@ -7,15 +7,36 @@ namespace InnoNum.UnitTests;
 
 public class PerfectNumberServiceUnitTest
 {
-    [Fact]
-    public void Test()
+    private readonly PerfectNumberService _service;
+
+    public PerfectNumberServiceUnitTest()
     {
         var mock = new Mock<ILogger<PerfectNumberService>>();
         var logger = mock.Object;
+        _service = new(logger);
+    }
 
-        var service = new PerfectNumberService(logger);
+    [Fact]
+    public void PerfectNumberCountingWorks()
+    {
         var expected = new PerfectNumberCount(1);
-        Assert.Equal(expected, service.CountPerfectNumbersBetween(1, 10));
+        var actual = _service.CountPerfectNumbersBetween(1, 10);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void MinimumNeedsToBeLessThanMaximum()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+            _service.CountPerfectNumbersBetween(10, 1));
+    }
+
+    [Fact]
+    public void AllArgumentsNeedToBeGreaterThanZero()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+            _service.CountPerfectNumbersBetween(-10, 0));
     }
 }
 
